bed022d741c8631c80a4557f5f76a477bd78d602
shrutiLetsgomo <shruti.aggarwal@letsgomo.com>
2015-03-18 19:42:18 +0530

    top length hardcoded

--- banner-lib/banner.js
+++ banner-lib/banner.js
@@ -1,31 +1,70 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	if(Banner_show) {
+		var banner = new Banner({
+			data:Banner_html_string
+		});
+		banner.initialize();
+	}
+});
+
 (function()
 {
-	var bannerSection = '';
-	Banner = function(){
-		this.data = Banner_html_string;
-		bannerSection = '<div id="banner_bottom" class="banner_bottom_fixed banner_bottom_hide">'+this.data+'</div>';
-		Banner.addBanner();
+	this.Banner = function(){
+		var defaults = {
+			data: Banner_html_string
+		}
+
+		if (arguments[0] && typeof arguments[0] === "object") {
+	      this.options = extendDefaults(defaults, arguments[0]);
+	    }
 	}
 
-	Banner.showBanner = function(){
+	Banner.prototype.initialize = function(){
+		buildUp.call(this);
+	    addBanner.call(this);
+		showBanner.call(this);
+		initializeEvents.call(this);
+	}
+
+	function extendDefaults(source, properties) {
+	    var property;
+	    for (property in properties) {
+	      if (properties.hasOwnProperty(property)) {
+	        source[property] = properties[property];
+	      }
+	    }
+	    return source;
+	}
+
+	function buildUp(){
+		if (typeof this.options.data === "string") {
+	      data = this.options.data;
+	    } else {
+	      data = this.options.data.innerHTML;
+	    }
+
+	}
+
+	function initializeEvents(){
+	 	window.onscroll = function() {
+			showBanner.call(this);
+		}
+	}
+
+	function addBanner(){
+
+		bannerSection = '<div id="banner_bottom" class="banner_bottom_fixed banner_bottom_hide">'+
+							this.options.data
+						+'</div>';
+		document.body.innerHTML += bannerSection;
+	}
+
+	function showBanner(){
 		var bannerBottomClassList = document.getElementById('banner_bottom').classList ; 
-		if(window.scrollY < Banner_show_after_Y) {
+		if(window.scrollY < 300) {
 			bannerBottomClassList.add('banner_bottom_hide');
 		} else {
 			bannerBottomClassList.remove('banner_bottom_hide');
 		}
 	}
-
-	Banner.addBanner = function(){
-		document.body.innerHTML += bannerSection;
-	}
-
-	if(Banner_show) {
-		new Banner();
-		Banner.showBanner();
-		window.onscroll = function() {
-			Banner.showBanner();
-		}
-	}
-
 })();
