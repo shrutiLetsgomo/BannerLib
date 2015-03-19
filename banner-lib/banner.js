d0b9924da96603ac831ef1459e61388216346a8b
shrutiLetsgomo <shruti.aggarwal@letsgomo.com>
2015-03-19 11:32:47 +0530

    replced documentcontentloaded with onready state and changed indentation.

--- banner-lib/banner.js
+++ banner-lib/banner.js
@@ -1,70 +1,68 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-	if(Banner_show) {
-		var banner = new Banner({
-			data:Banner_html_string
-		});
-		banner.initialize();
-	}
-});
+(function() {
+    document.onreadystatechange = function() {
+        if (document.readyState == "interactive" & Banner_show) {
+            var banner = new Banner({
+                data: Banner_html_string
+            });
+            banner.initialize();
+        }
+    }
 
-(function()
-{
-	this.Banner = function(){
-		var defaults = {
-			data: Banner_html_string
-		}
+    this.Banner = function() {
+        var defaults = {
+            data: Banner_html_string
+        }
 
-		if (arguments[0] && typeof arguments[0] === "object") {
-	      this.options = extendDefaults(defaults, arguments[0]);
-	    }
-	}
+        if (arguments[0] && typeof arguments[0] === "object") {
+            this.options = extendDefaults(defaults, arguments[0]);
+        }
+    }
 
-	Banner.prototype.initialize = function(){
-		buildUp.call(this);
-	    addBanner.call(this);
-		showBanner.call(this);
-		initializeEvents.call(this);
-	}
+    Banner.prototype.initialize = function() {
+        buildup.call(this);
+        addBanner.call(this);
+        showBanner.call(this);
+        initializeEvents.call(this);
+    }
 
-	function extendDefaults(source, properties) {
-	    var property;
-	    for (property in properties) {
-	      if (properties.hasOwnProperty(property)) {
-	        source[property] = properties[property];
-	      }
-	    }
-	    return source;
-	}
+    function extendDefaults(source, properties) {
+        var property;
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                source[property] = properties[property];
+            }
+        }
+        return source;
+    }
 
-	function buildUp(){
-		if (typeof this.options.data === "string") {
-	      data = this.options.data;
-	    } else {
-	      data = this.options.data.innerHTML;
-	    }
+    function buildup() {
+        if (typeof this.options.data === "string") {
+            data = this.options.data;
+        } else {
+            data = this.options.data.innerHTML;
+        }
 
-	}
+    }
 
-	function initializeEvents(){
-	 	window.onscroll = function() {
-			showBanner.call(this);
-		}
-	}
+    function initializeEvents() {
+        window.onscroll = function() {
+            showBanner.call(this);
+        }
+    }
 
-	function addBanner(){
+    function addBanner() {
 
-		bannerSection = '<div id="banner_bottom" class="banner_bottom_fixed banner_bottom_hide">'+
-							this.options.data
-						+'</div>';
-		document.body.innerHTML += bannerSection;
-	}
+        bannerSection = '<div id="banner_bottom" class="banner_bottom_fixed banner_bottom_hide">' +
+            this.options.data + '</div>';
+        document.body.innerHTML += bannerSection;
+    }
 
-	function showBanner(){
-		var bannerBottomClassList = document.getElementById('banner_bottom').classList ; 
-		if(window.scrollY < 300) {
-			bannerBottomClassList.add('banner_bottom_hide');
-		} else {
-			bannerBottomClassList.remove('banner_bottom_hide');
-		}
-	}
+    function showBanner() {
+        var bannerBottomClassList = document.getElementById('banner_bottom').classList;
+        if (window.scrollY < 300) {
+            bannerBottomClassList.add('banner_bottom_hide');
+        } else {
+            bannerBottomClassList.remove('banner_bottom_hide');
+        }
+    }
 })();
--- banner-lib/conf.js
+++ banner-lib/conf.js
@@ -1,2 +1,2 @@
-Banner_show = true ;
+Banner_show = true;
 Banner_html_string = "  £1.99 NEXT  DAY DELIVERY!  ENDS 9PM! USE CODE SPEEDY ";
